bd015fcc1ef919e9e22e5172aa267b0b8f7cf4a1
panditprogrammer <panditprogrammer@gmail.com>
2021-05-13 18:02:24 +0530

    snake game using javascript

---
+++ index.js
@@ -0,0 +1,177 @@
+let game = document.getElementById("game");
+let score_h1 = document.getElementById("scoreh1");
+let direction = { x: 0, y: 0 };
+let lastPrintTime = 0;
+let speed = 2;
+let snake_arr = [{ x: 13, y: 15 }];
+let snake_part, food_part, score = 0;
+let high_score;
+let food = { x: 12, y: 8 };
+
+// game sounds 
+let eat_sound = new Audio("assets/eat.wav");
+let high_score_sound = new Audio("assets/high_score.wav");
+let game_over_sound = new Audio("assets/game_over.wav");
+
+
+// game function 
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    if ((ctime - lastPrintTime) / 1000 < 1 / speed) {
+        return;
+    }
+    lastPrintTime = ctime;
+    gameEngine();
+    // console.log(ctime);
+
+}
+
+// collision detection
+function isCollide(snake) {
+    // if snake collide with its body 
+    for (let i = 1; i < snake_arr.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+            return true;
+        }
+
+    }
+    if (snake[0].x > 18 || snake[0].x < 0 || snake[0].y > 18 || snake[0].y < 0) {
+        return true;
+    }
+
+}
+
+function gameEngine() {
+
+    if (isCollide(snake_arr)) {
+        game_over_sound.play();
+        direction = { x: 0, y: 0 };
+        score = 0;
+        snake_arr = [{ x: 13, y: 15 }];
+        score_h1.innerText = `Score  ${score}`;
+
+    }
+
+    //    regenerate food and increment the snake 
+    if (snake_arr[0].y === food.y && snake_arr[0].x === food.x) {
+        // increment the score 
+        score += 10;
+        if (score % 200 === 0) {
+            speed = speed - 1;
+        }
+
+        console.log(speed);
+        eat_sound.play();
+
+        // localStorage set hight score 
+        if (score > high_score) {
+            high_score = score;
+            high_score_sound.play();
+            localStorage.setItem("high_score_local", JSON.stringify(high_score));
+            scorehigh.innerHTML = `Hight Score ${high_score}`;
+        }
+
+        score_h1.innerText = `Score  ${score}`;
+        snake_arr.unshift({ x: snake_arr[0].x + direction.x, y: snake_arr[0].y + direction.y });
+        let a = 1, b = 18;
+        food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
+    }
+
+    // moving the snake by one block
+    for (let i = snake_arr.length - 2; i >= 0; i--) {
+        snake_arr[i + 1] = { ...snake_arr[i] }; //new object of snake
+    }
+    // increment the head 
+    snake_arr[0].x += direction.x;
+    snake_arr[0].y += direction.y;
+
+
+
+    // console.log("x= ",snake_arr[0].x,"y = ",snake_arr[0].y);
+
+    game.innerHTML = "";
+
+    snake_arr.forEach((element, index) => {
+
+        // display the snake 
+        snake_part = document.createElement("div");
+        snake_part.style.gridRowStart = element.y;
+        snake_part.style.gridColumnStart = element.x;
+        // console.log(element.y,element.x);
+
+        // display head 
+        if (index === 0) {
+            snake_part.classList.add("head");
+        }
+        else //display body
+        {
+            snake_part.classList.add("snake");
+        }
+        game.appendChild(snake_part);
+
+    });
+
+    // display the food 
+    food_part = document.createElement("div");
+    food_part.style.gridRowStart = food.y;
+    food_part.style.gridColumnStart = food.x;
+    food_part.classList.add("food");
+    // console.log(food.y, food.x);
+    game.appendChild(food_part);
+}
+
+
+// high score store in localStorage 
+let high_score_local = localStorage.getItem("high_score_local");
+if (high_score_local === null) {
+    high_score = 0;
+    localStorage.setItem("high_score_local", JSON.stringify(high_score));
+}
+else {
+    high_score = JSON.parse(high_score_local);
+    scorehigh.innerText = `High score  ${high_score_local}`;
+}
+
+
+// main game logic 
+window.requestAnimationFrame(main);
+
+// keygame events handling 
+window.addEventListener("keydown", e => {
+
+
+    // change speed using user Selection 
+    let user_speed = document.getElementById('speed_user');
+    speed = parseInt(user_speed.value);
+    // console.log("value is ", speed);
+
+
+    direction = { x: 0, y: 1 } //start the game
+
+    // changing the snake direction
+    switch (e.key) {
+        case "ArrowUp":
+            direction.x = 0;
+            direction.y = -1;
+            break;
+        case "ArrowDown":
+            direction.x = 0;
+            direction.y = 1;
+
+            break;
+        case "ArrowLeft":
+            direction.x = -1;
+            direction.y = 0;
+
+            break;
+        case "ArrowRight":
+            direction.x = 1;
+            direction.y = 0;
+            break;
+
+        default:
+            break;
+    }
+
+
+});
