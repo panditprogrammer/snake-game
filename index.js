5571a9092a1c73254ac4628a38eabd55ff03ad55
panditprogrammer <panditprogrammer@gmail.com>
2021-05-14 11:02:59 +0530

    advanced snake game in javascipt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let game = document.getElementById("game");
 let score_h1 = document.getElementById("scoreh1");
+let game_over = document.querySelector(".game_over");
 let direction = { x: 0, y: 0 };
 let lastPrintTime = 0;
 let speed = 2;
@@ -12,6 +13,10 @@ let food = { x: 12, y: 8 };
 let eat_sound = new Audio("assets/eat.wav");
 let high_score_sound = new Audio("assets/high_score.wav");
 let game_over_sound = new Audio("assets/game_over.wav");
+
+
+// start display 
+game_over.innerHTML = ` <strong>Press Arrow key to Play</strong>`;
 
 
 // game function 
@@ -35,7 +40,8 @@ function isCollide(snake) {
         }
 
     }
-    if (snake[0].x > 18 || snake[0].x < 0 || snake[0].y > 18 || snake[0].y < 0) {
+    // checking wall collision 
+    if (snake[0].x > 20 || snake[0].x < 0 || snake[0].y > 20 || snake[0].y < 0) {
         return true;
     }
 
@@ -43,24 +49,29 @@ function isCollide(snake) {
 
 function gameEngine() {
 
+    // if game over after collision 
     if (isCollide(snake_arr)) {
         game_over_sound.play();
         direction = { x: 0, y: 0 };
+        game_over.style.display = "block";
+        game_over.innerHTML = `  <h3>Game Over! </h3>
+        <strong>Press Arrow key to play Again</strong>`;
         score = 0;
         snake_arr = [{ x: 13, y: 15 }];
         score_h1.innerText = `Score  ${score}`;
 
+
     }
 
-    //    regenerate food and increment the snake 
+    //    eating food and increment the snake 
     if (snake_arr[0].y === food.y && snake_arr[0].x === food.x) {
         // increment the score 
         score += 10;
         if (score % 200 === 0) {
-            speed = speed - 1;
+            speed = speed - 2;
         }
 
-        console.log(speed);
+        // console.log(speed);
         eat_sound.play();
 
         // localStorage set hight score 
@@ -72,8 +83,9 @@ function gameEngine() {
         }
 
         score_h1.innerText = `Score  ${score}`;
+        // food generating 
         snake_arr.unshift({ x: snake_arr[0].x + direction.x, y: snake_arr[0].y + direction.y });
-        let a = 1, b = 18;
+        let a = 1, b = 20;
         food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
     }
 
@@ -128,10 +140,10 @@ if (high_score_local === null) {
     localStorage.setItem("high_score_local", JSON.stringify(high_score));
 }
 else {
+    // high_score from localStorage 
     high_score = JSON.parse(high_score_local);
     scorehigh.innerText = `High score  ${high_score_local}`;
 }
-
 
 // main game logic 
 window.requestAnimationFrame(main);
@@ -139,6 +151,8 @@ window.requestAnimationFrame(main);
 // keygame events handling 
 window.addEventListener("keydown", e => {
 
+    // game_over dialog hide 
+    game_over.style.display = "none";
 
     // change speed using user Selection 
     let user_speed = document.getElementById('speed_user');
@@ -173,5 +187,36 @@ window.addEventListener("keydown", e => {
             break;
     }
 
+});
+
+// for right array key for smartphone 
+window.addEventListener("click", () => {
+
+    // game_over dialog hide 
+    game_over.style.display = "none";
+    // change speed using user Selection 
+    let user_speed = document.getElementById('speed_user');
+    speed = parseInt(user_speed.value);
+    // console.log("value is ", speed);
+
+    right.addEventListener('click', () => {
+        direction.x = 1;
+        direction.y = 0;
+    });
+
+    left.addEventListener('click', () => {
+        direction.x = -1;
+        direction.y = 0;
+    });
+
+    up.addEventListener('click', () => {
+        direction.x = 0;
+        direction.y = -1;
+    });
+
+    down.addEventListener('click', () => {
+        direction.x = 0;
+        direction.y = 1;
+    });
 
 });
